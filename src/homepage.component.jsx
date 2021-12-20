5eb223709e3fcde9b6c666cd1d26f22ce24911f1
Sébastien bellon <sebastien.bellon@nagra.com>
2021-12-20 09:04:36 +0100

    add scss for homepage

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,62 +1,46 @@
-import React from 'react'
+import React from "react";
 
+import "./homepage.styles.scss";
 
 const HomePage = () => (
-    <div>
-        <div className='homepage'>
-            <div className='directory-menu'>
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>Hat</h1>
-                        <span className='subtitle'>SHOP NOW</span>
-                    </div>
-                </div>
-            </div>
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Hat</h1>
+          <span className="subtitle">SHOP NOW</span>
         </div>
-        <div className='homepage'>
-            <div className='directory-menu'>
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>Jacket</h1>
-                        <span className='subtitle'>SHOP NOW</span>
-                    </div>
-                </div>
-            </div>
-        </div>
+      </div>
 
-        <div className='homepage'>
-            <div className='directory-menu'>
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>SNEAKERS</h1>
-                        <span className='subtitle'>SHOP NOW</span>
-                    </div>
-                </div>
-            </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Jacket</h1>
+          <span className="subtitle">SHOP NOW</span>
         </div>
+      </div>
 
-        <div className='homepage'>
-            <div className='directory-menu'>
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>WOMENS</h1>
-                        <span className='subtitle'>SHOP NOW</span>
-                    </div>
-                </div>
-            </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">SNEAKERS</h1>
+          <span className="subtitle">SHOP NOW</span>
         </div>
+      </div>
 
-        <div className='homepage'>
-            <div className='directory-menu'>
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>MENS</h1>
-                        <span className='subtitle'>SHOP NOW</span>
-                    </div>
-                </div>
-            </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">WOMENS</h1>
+          <span className="subtitle">SHOP NOW</span>
         </div>
+      </div>
+
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">MENS</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
     </div>
+  </div>
 );
 
 export default HomePage;

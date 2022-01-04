c465ae6dc45f6cce980006892bf736e0052b9405
Sébastien bellon <sebastien.bellon@nagra.com>
2022-01-04 11:12:38 +0100

    update collection item

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import "./App.css";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
+
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route exact path="/" element={<HomePage />} />
+          <Route path="/shop" element={<ShopPage />} />
         </Routes>
       </BrowserRouter>
     </div>

7c701af6a7226de925ac6654480bccf78f0d0973
Sébastien bellon <sebastien.bellon@nagra.com>
2022-01-05 18:34:57 +0100

    add cart-item to cart dropdown menu

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,17 +1,27 @@
 import React from "react";
 
+import { connect } from "react-redux";
+
 import "./cart-dropdown.style.scss";
 
 import CustomButton from "../custom-button/custom-button.component";
+import CartItem from "../cart-item/cart-item.component";
 
-const CartDropDown = () => {
-    return (
-        <div className="cart-dropdown">
-            <div className="cart-items">
-            <CustomButton>GO TO CHECKOUT</CustomButton>
-            </div>
-        </div>
-    );
+const CartDropDown = ({ cartItems }) => {
+  return (
+    <div className="cart-dropdown">
+      <div className="cart-items">
+        {cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))}
+      </div>
+      <CustomButton>GO TO CHECKOUT</CustomButton>
+    </div>
+  );
 };
 
-export default CartDropDown;
+const mapStateToProps = (state) => ({
+  cartItems: state.cart.cartItems,
+});
+
+export default connect(mapStateToProps, null)(CartDropDown);
